3a58d7693cbf53bb9f1feebb1982d2521be0fd14
Elghandour-eng <abdelwahabhassan2000@gmail.com>
2023-08-24 00:15:38 +0300

    update url of logo in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,9 +15,12 @@
             {{-- <img src="{{ asset(getAppLogo()) }}" alt="Logo" class="object-cover sidebar-app-logo"/> --}}
 
         {{-- </button> --}}
-
+ 
+        <!-- Source Of Logo Image In Side Bar DashBoard Is Uploaded To Fire Base And That Is The Link Bleow -->
+        <!-- https://firebasestorage.googleapis.com/v0/b/menialwyschool.appspot.com/o/302003776_212652184434245_4474682807937351330_n-removebg-preview.png?alt=media&token=41db073c-600b-4ff8-be6c-6223445c7a2f -->
+        <!-- Using The Shorten Url Tool To Make The Link = "https://shorturl.at/adfJL" -->
         <button type="button" class="btn px-0 aside-menu-container__aside-menubar d-lg-block d-none sidebar-btn m-auto">
-                       <span class="aside-menu-icon pe-3"><img  src="https://i.ibb.co/d09jX9Y/logo-removebg-preview.png" alt=""  width="30px" height="50px"></span>
+                       <span class="aside-menu-icon pe-3"><img  src="https://shorturl.at/adfJL" alt=""  width="30px" height="50px"></span>
             <span class="aside-menu-title title-dash"></span>
          
         </button>
