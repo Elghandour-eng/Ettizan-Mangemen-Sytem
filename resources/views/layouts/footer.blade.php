46c8307944665a5a87b292288a554c2080964276
Ahmed Ibrahem <ahmed.ibrahem267200000@gmail.com>
2023-09-01 10:00:45 +0300

    Initial commit of old version

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container-fluid padding-0">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-xl-6">
+                <div class="copyright text-center text-muted">
+                    {{ __('messages.all_rights_reserved') }} &copy; {{ date('Y') }} <a href="http://Visionclubkw.com" target="_blank"
+                                                                  class="font-weight-bold ml-1"
+                                                                      >Vision Club</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
